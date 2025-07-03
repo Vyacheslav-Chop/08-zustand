99ba7acd33aec77d80ad416bf93d765cdc506e30
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-07-03 19:06:23 +0300

    add metategs

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,30 @@
 import Link from "next/link";
 import css from "./not-found.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+  description: "404 Not Found — The requested page could not be found.",
+  openGraph: {
+    title: "Not Found (404)",
+    description: "404 Not Found — The requested page could not be found.",
+    url: "https://08-zustand-seven.vercel.app/",
+    images: [
+      {
+        url: "/not_found.png",
+        width: 1200,
+        height: 630,
+        alt: "Not Found",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Not Found (404)",
+    description: "404 Not Found — The requested page could not be found.",
+    images: ["/not_found.png"],
+  },
+};
 
 export default function NotFound() {
   return (
